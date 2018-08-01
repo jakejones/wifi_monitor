30df1728cf1eb0a89ded97e7e9ad4fc3fa700e24
jakejones <jakeljones95@gmail.com>
2018-08-01 20:00:46 +0800

    Prevent crash if log file is corrupt.

--- analysis/device_list.py
+++ analysis/device_list.py
@@ -11,17 +11,24 @@ data_path = sys.argv[1]
 
 # -- Read Log File --
 activity = {}
+i = 0
 with open(data_path,'r') as logfile:
     for line in logfile:
-        time, mac = line.split()
-        if mac in activity:
-            activity[mac] += 1
-        else:
-            activity[mac] = 1 
+        try:
+            time, mac = line.split()
+            if mac in activity:
+                activity[mac] += 1
+            else:
+                activity[mac] = 1
+            i = i + 1
+        except:
+            #print "Invalid Line:", i
+            pass
 logfile.close()
 
-
+print "Number of detected devices =", len(activity)
 for mac in activity:
     vendor = requests.get('http://macvendors.co/api/vendorname/' + mac )
     info = ["MAC: "+mac,"Frames: "+str(activity[mac]),"Vendor: "+vendor.text]
+    #info = ["MAC: "+mac,"Frames: "+str(activity[mac])]
     print "\t".join(info)
